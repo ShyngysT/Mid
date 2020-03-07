33696790675181e87fc18c4f12d015159b32a924
ShyngysT <tagayshyngys@gmail.com>
2020-03-07 18:12:09 +0600

    Initial commit

---
+++ Mid/Blue.swift
@@ -0,0 +1,27 @@
+//
+//  Blue.swift
+//  Mid
+//
+//  Created by Tagay Shyngys on 3/7/20.
+//  Copyright © 2020 Tagay Shyngys. All rights reserved.
+//
+
+import UIKit
+
+class Blue: NSObject {
+    @IBAction func b4(_ sender: Any) {
+    }
+    @IBAction func b3(_ sender: Any) {
+    }
+    @IBAction func b2(_ sender: Any) {
+    }
+    @IBAction func b1(_ sender: Any) {
+    }
+    
+}
+
+    
+   
+    
+    
+
---
+++ Mid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mid
+//
+//  Created by Tagay Shyngys on 3/7/20.
+//  Copyright © 2020 Tagay Shyngys. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
